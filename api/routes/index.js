f6e081987975bbefebab0d429782d3c96764d799
Abhimanyue Singh Tanwar <abhimanyu891998@users.noreply.github.com>
2018-10-18 14:06:31 +0800

    signup login landing complete

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,22 +7,33 @@ var user_controller = require('../controllers/userController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('signUpPage', { title: 'Express' });
 });
 
-router.post('/signup', user_controller.user_sign_up)
+/*router.post('/signup', function(req, res, next) {
+  res.render('signInPage', { title: 'Express' })
+});*/
 
 
-router.get('/signupPage',function(req,res,next){
+/*router.get('/signupPage',function(req,res,next){
 
   res.render('signUpPage');
 
+});*/
+
+router.get('/landing',function(req,res,next){
+res.render('index');
 });
 
-router.get('/login', login_controller.login_attempt)
+
+router.get('/signin', function(req,res,next){
+res.render('signInPage');
+});
 
 
-router.post('/signupPost', function(req,res,next){
+
+
+router.get('/signup', function(req,res,next){
 
     res.render('signInPage');
 
