70bdfa875ecb290140d7937dcd72986db1016bed
haleyk10198 <haleyk100198@gmail.com>
2018-10-18 15:30:27 +0800

    build

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/displayMap', function(req, res, next){
+    req.db.getCollection('trends').find({}, function(err, records){
+        if(err){
+            res.send({msg: err});
+            return;
+        }
+        res.send(records);
+        return;
+    })
+});
+
+req.db.getCollection('searchLog').update({})
+
+req.db.find()
+
 module.exports = router;
