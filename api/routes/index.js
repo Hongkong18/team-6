c55af8f5e29de33e60622e462aaeb2515d7e30a7
Abhimanyue Singh Tanwar <abhimanyu891998@users.noreply.github.com>
2018-10-18 12:04:47 +0800

    signuppage route added

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/signupPage',function(req,res,next){
+  
+  res.render('signUpPage');
+
+});
+
+
+
+
+
 module.exports = router;
---
+++ api/routes/signupPage.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router  = express.Router();
+
+router.get('/signup', function(req,res,next)
+{
+
+    res.render('signUpPage');
+
+});
+
+router.post('')
+
+
